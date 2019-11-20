d2931a70e027ceeac5147db8e14a4490f6739f08
Shahzad Ahsan <dankara97@gmail.com>
2019-11-20 18:20:16 +0500

    make some modules files

---
+++ options.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+rate = 44100
+freq =19100# chosen because it is outside most people's hearing and worked for my microphone and speakers
+channels = 1
+frame_length = 3
+chunk = 256
+datasize = chunk * frame_length
+sigil = "00"
+
